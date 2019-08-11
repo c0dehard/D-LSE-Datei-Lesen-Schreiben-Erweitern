4d50987bcb850aa3f6b54be52b72831c852f1662
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-08-11 02:16:02 +0200

    Add files via upload

---
+++ D-LSE.py.py
@@ -0,0 +1,39 @@
+import os
+programRuns = True
+
+def read():
+    print("Lesemodus[ "+datei+" ]\n")
+    print(f.read())
+
+def write():
+    f.write(str(input("Schreibmodus[ "+datei+" ]\n")))
+
+
+def append():
+    f.write(str(input("Erweiterungsmodus [ "+datei+" ]\n")))
+
+while programRuns:
+    os.system('cls')
+    datei = str(input("Pfad oder Dateiname\n~> "))
+    modus = str(input("'r' read | 'w' write | 'a' append\n"))
+    f = open (datei,modus)
+    if modus == "r": #ergab fehler mit ''
+        read()
+    elif modus == "w":
+        write()
+    elif modus == "a":
+        append()
+    weiter = input("\nWeitermachen? j/n\n")
+    if weiter == 'n':
+        f.close()
+        print('Danke, bis demnächst!')
+        programRuns = False
+    elif weiter =='j':
+        f.close()
+    else print('Ungültige eingabe')
+os.system('cls')#bei macos/unix/linux auf 'clear' ändern
+
+
+
+
+
